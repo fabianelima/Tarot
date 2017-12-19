b81e359e4c34934deb124eb1fa90f41a4d892293
fabianelima <fabianelim@gmail.com>
2017-12-19 15:50:58 -0200

    Consulta
    A parte de consulta ao "acervo" já está pronta. Ainda preciso decidir se
    é melhor embaralhar as cartas primeiro ou escolher o tipo de leitura
    antes de embaralhar.

--- script.coffee
+++ script.coffee
@@ -1,6 +1,29 @@
 $ ->
+	cards = [	# São 78 cartas no total, 22 arcanos maiores. cada carta terá sua 'ficha'. Vou começar com 5 cartas só pra ver se funciona.
+				{
+					name: 'carta 1'
+					desc: 'Lorem ipsum dolor sit ammet'
+				}
+				{
+					name: 'carta 2'
+					desc: 'Lorem ipsum dolor sit ammet'
+				}
+				{
+					name: 'carta 3'
+					desc: 'Lorem ipsum dolor sit ammet'
+				}
+				{
+					name: 'carta 4'
+					desc: 'Lorem ipsum dolor sit ammet'
+				}
+				{
+					name: 'carta 5'
+					desc: 'Lorem ipsum dolor sit ammet'
+				}
+			]
 	nav =
 		consult: ->
+			func.allCards()
 			$('main section').fadeOut()
 			$('main section:nth-child(2)').delay(400).fadeIn()
 
@@ -11,8 +34,24 @@ $ ->
 		back: ->
 			$('main section').fadeOut()
 			$('main section:nth-child(1)').delay(400).fadeIn()
+			$('main section:nth-child(2) .cards').html('')
+
+		close: -> $('.modal').fadeOut()
+
+	func =
+		allCards: ->
+			j = 0
+			cards.forEach ->
+				$('main section:nth-child(2) .cards').append('<div class="card" id="c' + j + '">' + cards[j].name + '</div>')
+				j++
+
+		seeCard: ($el) ->
+			$('.modal').fadeIn()
+			$('.modal').html('<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1><p>' + cards[Number($el.attr('id').slice(1))].desc + '</p><button class="close">Fechar</button>')
 
 	# eventos
 	$(document).on 'click', '.consult', -> nav.consult()
 	$(document).on 'click', '.reading', -> nav.reading()
 	$(document).on 'click', '.back', -> nav.back()
+	$(document).on 'click', '.close', -> nav.close()
+	$(document).on 'click', '.card', -> func.seeCard $(this)
--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 (function() {
   $(function() {
-    var nav;
+    var cards, func, nav;
+    cards = [
+      {
+        name: 'carta 1',
+        desc: 'Lorem ipsum dolor sit ammet'
+      }, {
+        name: 'carta 2',
+        desc: 'Lorem ipsum dolor sit ammet'
+      }, {
+        name: 'carta 3',
+        desc: 'Lorem ipsum dolor sit ammet'
+      }, {
+        name: 'carta 4',
+        desc: 'Lorem ipsum dolor sit ammet'
+      }, {
+        name: 'carta 5',
+        desc: 'Lorem ipsum dolor sit ammet'
+      }
+    ];
     nav = {
       consult: function() {
+        func.allCards();
         $('main section').fadeOut();
         return $('main section:nth-child(2)').delay(400).fadeIn();
       },
@@ -12,7 +31,25 @@
       },
       back: function() {
         $('main section').fadeOut();
-        return $('main section:nth-child(1)').delay(400).fadeIn();
+        $('main section:nth-child(1)').delay(400).fadeIn();
+        return $('main section:nth-child(2) .cards').html('');
+      },
+      close: function() {
+        return $('.modal').fadeOut();
+      }
+    };
+    func = {
+      allCards: function() {
+        var j;
+        j = 0;
+        return cards.forEach(function() {
+          $('main section:nth-child(2) .cards').append('<div class="card" id="c' + j + '">' + cards[j].name + '</div>');
+          return j++;
+        });
+      },
+      seeCard: function($el) {
+        $('.modal').fadeIn();
+        return $('.modal').html('<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1><p>' + cards[Number($el.attr('id').slice(1))].desc + '</p><button class="close">Fechar</button>');
       }
     };
     $(document).on('click', '.consult', function() {
@@ -21,8 +58,14 @@
     $(document).on('click', '.reading', function() {
       return nav.reading();
     });
-    return $(document).on('click', '.back', function() {
+    $(document).on('click', '.back', function() {
       return nav.back();
+    });
+    $(document).on('click', '.close', function() {
+      return nav.close();
+    });
+    return $(document).on('click', '.card', function() {
+      return func.seeCard($(this));
     });
   });
 
