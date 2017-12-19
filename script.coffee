7c0f0725f2a1e35dc3e0cdf31e8660a8f12755e3
fabianelima <fabianelim@gmail.com>
2017-12-19 16:45:09 -0200

    Quase
    Acho. Preciso arrumar ainda como fazer o botão de voltar zerar todas as
    preferências.

--- script.coffee
+++ script.coffee
@@ -1,4 +1,11 @@
 $ ->
+	ctrl = []
+	reading = {
+		isReading: true
+		numCards: 0
+		cardsClicked: 0
+		cardsReading: []
+	}
 	cards = [	# São 78 cartas no total, 22 arcanos maiores. cada carta terá sua 'ficha'. Vou começar com 5 cartas só pra ver se funciona.
 				{
 					name: 'carta 1'
@@ -32,9 +39,11 @@ $ ->
 			$('main section:nth-child(3)').delay(400).fadeIn()
 
 		back: ->
+			reading.isReading = false
 			$('main section').fadeOut()
 			$('main section:nth-child(1)').delay(400).fadeIn()
 			$('main section:nth-child(2) .cards').html('')
+			$('main section:nth-child(3) .cards').html('')
 
 		close: -> $('.modal').fadeOut()
 
@@ -49,9 +58,70 @@ $ ->
 			$('.modal').fadeIn()
 			$('.modal').html('<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1><p>' + cards[Number($el.attr('id').slice(1))].desc + '</p><button class="close">Fechar</button>')
 
+		rNumber: ->
+			randy = Math.floor(Math.random() * cards.length)
+
+			if ctrl.length < cards.length
+				if ctrl.indexOf(randy) is -1
+					ctrl.push randy
+
+				func.rNumber()
+
+		shuffle: ->
+			func.rNumber()
+
+			for i in ctrl
+				$('main section:nth-child(3) .cards').append('<div class="card-r" id="c' + i + '">' + cards[i].name + '</div>')
+
+			$('.modal').fadeIn().html('
+				<h1>Escolha o tipo de leitura</h1>
+				<button class="read1">Uma carta</button>
+				<button class="read2">Duas cartas</button>
+				<button class="read3">Três cartas</button>
+			')
+
+		numberCards: ($el) ->
+			reading.isReading = true
+			reading.numCards = Number($el.attr('class').slice(4))
+			$('.modal, .shuffle').fadeOut()
+
+			if $el.attr('class') is 'read1' then $('main section:nth-child(3) p').html('Clique sobre uma carta:')
+			if $el.attr('class') is 'read2' then $('main section:nth-child(3) p').html('Clique sobre duas cartas:')
+			if $el.attr('class') is 'read3' then $('main section:nth-child(3) p').html('Clique sobre três cartas:')
+
+		choice: ($el) ->
+			reading.cardsClicked++
+
+			if reading.cardsClicked <= reading.numCards
+				reading.cardsReading.push Number($el.attr('id').slice(1))
+
+				if reading.numCards is 1
+					$('main section:nth-child(3) .cards').html('
+						<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1>
+						<p>' + cards[Number($el.attr('id').slice(1))].desc + '</p>
+					')
+
+				if reading.numCards > 1
+					console.log 'oioi', reading.cardsReading, reading.numCards
+					if reading.numCards is reading.cardsClicked
+						$('main section:nth-child(3) .cards').html('')
+
+						k = 0
+						reading.cardsReading.forEach ->
+							$('main section:nth-child(3) .cards').append('
+								<h1>' + cards[reading.cardsReading[k]].name + '</h1>
+								<p>' + cards[reading.cardsReading[k]].desc + '</p>
+							')
+							k++
+			else
+				$('.card-r').css { pointerEvents: 'none' }
+
 	# eventos
 	$(document).on 'click', '.consult', -> nav.consult()
 	$(document).on 'click', '.reading', -> nav.reading()
 	$(document).on 'click', '.back', -> nav.back()
 	$(document).on 'click', '.close', -> nav.close()
 	$(document).on 'click', '.card', -> func.seeCard $(this)
+	$(document).on 'click', '.shuffle', -> func.shuffle()
+	$(document).on 'click', '.read1, .read2, .read3', -> func.numberCards $(this)
+	$(document).on 'click', '.card-r', -> func.choice $(this)
--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 (function() {
   $(function() {
-    var cards, func, nav;
+    var cards, ctrl, func, nav, reading;
+    ctrl = [];
+    reading = {
+      isReading: true,
+      numCards: 0,
+      cardsClicked: 0,
+      cardsReading: []
+    };
     cards = [
       {
         name: 'carta 1',
@@ -30,9 +37,11 @@
         return $('main section:nth-child(3)').delay(400).fadeIn();
       },
       back: function() {
+        reading.isReading = false;
         $('main section').fadeOut();
         $('main section:nth-child(1)').delay(400).fadeIn();
-        return $('main section:nth-child(2) .cards').html('');
+        $('main section:nth-child(2) .cards').html('');
+        return $('main section:nth-child(3) .cards').html('');
       },
       close: function() {
         return $('.modal').fadeOut();
@@ -50,6 +59,64 @@
       seeCard: function($el) {
         $('.modal').fadeIn();
         return $('.modal').html('<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1><p>' + cards[Number($el.attr('id').slice(1))].desc + '</p><button class="close">Fechar</button>');
+      },
+      rNumber: function() {
+        var randy;
+        randy = Math.floor(Math.random() * cards.length);
+        if (ctrl.length < cards.length) {
+          if (ctrl.indexOf(randy) === -1) {
+            ctrl.push(randy);
+          }
+          return func.rNumber();
+        }
+      },
+      shuffle: function() {
+        var i, l, len;
+        func.rNumber();
+        for (l = 0, len = ctrl.length; l < len; l++) {
+          i = ctrl[l];
+          $('main section:nth-child(3) .cards').append('<div class="card-r" id="c' + i + '">' + cards[i].name + '</div>');
+        }
+        return $('.modal').fadeIn().html('<h1>Escolha o tipo de leitura</h1> <button class="read1">Uma carta</button> <button class="read2">Duas cartas</button> <button class="read3">Três cartas</button>');
+      },
+      numberCards: function($el) {
+        reading.isReading = true;
+        reading.numCards = Number($el.attr('class').slice(4));
+        $('.modal, .shuffle').fadeOut();
+        if ($el.attr('class') === 'read1') {
+          $('main section:nth-child(3) p').html('Clique sobre uma carta:');
+        }
+        if ($el.attr('class') === 'read2') {
+          $('main section:nth-child(3) p').html('Clique sobre duas cartas:');
+        }
+        if ($el.attr('class') === 'read3') {
+          return $('main section:nth-child(3) p').html('Clique sobre três cartas:');
+        }
+      },
+      choice: function($el) {
+        var k;
+        reading.cardsClicked++;
+        if (reading.cardsClicked <= reading.numCards) {
+          reading.cardsReading.push(Number($el.attr('id').slice(1)));
+          if (reading.numCards === 1) {
+            $('main section:nth-child(3) .cards').html('<h1>' + cards[Number($el.attr('id').slice(1))].name + '</h1> <p>' + cards[Number($el.attr('id').slice(1))].desc + '</p>');
+          }
+          if (reading.numCards > 1) {
+            console.log('oioi', reading.cardsReading, reading.numCards);
+            if (reading.numCards === reading.cardsClicked) {
+              $('main section:nth-child(3) .cards').html('');
+              k = 0;
+              return reading.cardsReading.forEach(function() {
+                $('main section:nth-child(3) .cards').append('<h1>' + cards[reading.cardsReading[k]].name + '</h1> <p>' + cards[reading.cardsReading[k]].desc + '</p>');
+                return k++;
+              });
+            }
+          }
+        } else {
+          return $('.card-r').css({
+            pointerEvents: 'none'
+          });
+        }
       }
     };
     $(document).on('click', '.consult', function() {
@@ -64,8 +131,17 @@
     $(document).on('click', '.close', function() {
       return nav.close();
     });
-    return $(document).on('click', '.card', function() {
+    $(document).on('click', '.card', function() {
       return func.seeCard($(this));
+    });
+    $(document).on('click', '.shuffle', function() {
+      return func.shuffle();
+    });
+    $(document).on('click', '.read1, .read2, .read3', function() {
+      return func.numberCards($(this));
+    });
+    return $(document).on('click', '.card-r', function() {
+      return func.choice($(this));
     });
   });
 
